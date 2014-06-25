19839c36c6fa8e8cdf14d70179846c9d59721c60
Haowei <haowei.hsieh@cengage.com>
2014-06-25 09:29:26 -0400

    working as a browser action

--- deselect.js
+++ deselect.js
@@ -1,4 +1,4 @@
 // Called when the user clicks on the browser action icon.
 chrome.browserAction.onClicked.addListener(function() {
-    chrome.tabs.executeScript(null, {file: "content_script.js"});
+	chrome.tabs.executeScript(null, {file: "content_script.js"});
 });
